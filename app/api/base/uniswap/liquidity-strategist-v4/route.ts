3013a63b86c3b50ebef4e910dff426d574506409
oji3.eth <95267643+open3x@users.noreply.github.com>
2025-06-10 12:49:58 +0800

    fix (#8)

--- app/api/base/uniswap/liquidity-strategist-v4/route.ts
+++ app/api/base/uniswap/liquidity-strategist-v4/route.ts
@@ -3,36 +3,49 @@ import { isAddress } from "viem";
 import { createSignature } from "@/lib/signature";
 import { client } from "@/lib/rpc/client";
 
-const ABI = [{
-  "inputs": [{ "name": "account", "type": "address" }],
+const PositionManager = "0x7C5f5A4bBd8fD63184577525326123B519429bDc";
+const ABI = [  {
+  "inputs": [
+    {
+      "internalType": "address",
+      "name": "owner",
+      "type": "address"
+    }
+  ],
   "name": "balanceOf",
-  "outputs": [{ "name": "", "type": "uint256" }],
+  "outputs": [
+    {
+      "internalType": "uint256",
+      "name": "",
+      "type": "uint256"
+    }
+  ],
   "stateMutability": "view",
   "type": "function"
-}] as const;
+},] as const;
 
-export async function GET(req: NextRequest, { params: { token } }: { params: { token: string } }) {
+export async function GET(req: NextRequest) {
   try {
     const address = req.nextUrl.searchParams.get("address");
     if (!address || !isAddress(address)) {
       return new Response("Invalid address",{ status: 400 });
     }
-    if (!token || !isAddress(token)) {
-      return new Response("Invalid token",{ status: 400 });
-    }
 
     const balance = await client.readContract({
-      address: token,
+      address: PositionManager,
       abi: ABI,
       functionName: "balanceOf",
       args: [address],
     });
-    const mint_eligibility = balance > BigInt(0);
-    const signature = await createSignature({ address, mint_eligibility });
+    const mint_eligibility = balance >= BigInt(5);
+    const data = balance.toString();
+    const signature = await createSignature({ address, mint_eligibility, data });
 
-    return Response.json({ signature, mint_eligibility }, { status: 200 });
+    return Response.json({ signature, mint_eligibility, data }, { status: 200 });
   } catch (err) {
     console.error(err);
     return new Response("Internal server error", { status: 500 });
   }
 }
+
+
